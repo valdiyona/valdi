beabdf64256713f04d25a80eee00f843f77c8688
valdiyona <42940572+valdiyona@users.noreply.github.com>
2018-09-04 17:08:11 +0700

    Add files via upload

---
+++ TUGAS 1 - VALDI - D3 TT 3A -23/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author valdi
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int jumlah = 5;
+        // TODO code application logic here
+        for (int i=0; i<jumlah; i++){
+                for(int j=0; j<=i; j++){
+                System.out.print("X");
+                }
+                System.out.print("\n");
+        }
+                System.out.print("\n");
+    }
+    
+}
